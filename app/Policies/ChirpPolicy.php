13a9ecf850b5235e88cf3e56000e356806ed1ec9
3B032062 <3B032062@gm.student.ncut.edu.tw>
2023-12-25 00:13:35 +0800

    Authorization

---
+++ app/Policies/ChirpPolicy.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Chirp;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ChirpPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function viewAny(User $user):bool
+    {
+        //
+    }
+
+    public function update(User $user,Chirp $chirp):bool
+    {
+        return $chirp->user()->is($user);
+    }
+}
